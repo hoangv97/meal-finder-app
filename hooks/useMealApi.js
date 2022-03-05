6c78db5e048109bb3a26c9a6b25bcd88645bf582
Viet Hoang <ngviethoang0212@gmail.com>
2022-03-05 19:43:46 +0700

    refactor api

--- hooks/useMealApi.js
+++ hooks/useMealApi.js
@@ -1,58 +1,48 @@
 import useFetch from 'react-fetch-hook';
 import axios from 'axios';
 
+const API_PREFIX = 'https://www.themealdb.com/api/json/v1/1/';
+
 export const getMealsByName = (name) => {
-  return axios.get(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
-  );
+  return axios.get(`${API_PREFIX}search.php?s=${name}`);
 };
 
 export const getMealsByFirstLetter = (name) => {
-  return axios.get(
-    `https://www.themealdb.com/api/json/v1/1/search.php?f=${name}`
-  );
+  return axios.get(`${API_PREFIX}search.php?f=${name}`);
 };
 
 export const getMealById = (id) => {
-  return axios.get(
-    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-  );
+  return axios.get(`${API_PREFIX}lookup.php?i=${id}`);
 };
 
 export const getRandomMeal = () => {
-  return axios.get(`https://www.themealdb.com/api/json/v1/1/random.php`);
+  return axios.get(`${API_PREFIX}random.php`);
 };
 
 export const getAllMealCategories = () => {
-  return axios.get(`https://www.themealdb.com/api/json/v1/1/categories.php`);
+  return axios.get(`${API_PREFIX}categories.php`);
 };
 
 export const getAllCategories = () => {
-  return axios.get(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`);
+  return axios.get(`${API_PREFIX}list.php?c=list`);
 };
 
 export const getAllAreas = () => {
-  return axios.get(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`);
+  return axios.get(`${API_PREFIX}list.php?a=list`);
 };
 
 export const getAllIngredients = () => {
-  return axios.get(`https://www.themealdb.com/api/json/v1/1/list.php?i=list`);
+  return axios.get(`${API_PREFIX}list.php?i=list`);
 };
 
 export const getMealsByMainIngredient = (ingredient) => {
-  return axios.get(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
-  );
+  return axios.get(`${API_PREFIX}filter.php?i=${ingredient}`);
 };
 
 export const getMealsByCategory = (category) => {
-  return axios.get(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
-  );
+  return axios.get(`${API_PREFIX}filter.php?c=${category}`);
 };
 
 export const getMealsByArea = (area) => {
-  return axios.get(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`
-  );
+  return axios.get(`${API_PREFIX}filter.php?a=${area}`);
 };
